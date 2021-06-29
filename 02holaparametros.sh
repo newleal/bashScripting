60aa97cb5abe35ecb5ee81322dac9ce7f34c8c76
miguelleal33 <miguelangel1985rojo@gmail.com>
2021-06-28 20:01:43 -0500

    Script que muestra los parametros introducidos

---
+++ 02holaparametros.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+echo "El script $0"
+
+echo "Recibe argumentos $1 $2 $3 $4"
+echo "Argumentos recibidos son: $@"
+echo "Argumentos recibidos son: $*"
+echo "La cantidad de argumentos ingresados $#"
